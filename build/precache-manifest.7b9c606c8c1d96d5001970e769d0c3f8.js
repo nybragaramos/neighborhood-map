bb5bbea9446790cb678ce587195186ee62064ca6
nadjayanna <nadjayannabr@gmail.com>
2018-11-15 17:30:46 +0100

    Add readme

---
+++ build/precache-manifest.7b9c606c8c1d96d5001970e769d0c3f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "743c15a7856f640e43f2",
+    "url": "/static/js/main.743c15a7.chunk.js"
+  },
+  {
+    "revision": "e08eda4bdb643a898e4b",
+    "url": "/static/js/1.e08eda4b.chunk.js"
+  },
+  {
+    "revision": "743c15a7856f640e43f2",
+    "url": "/static/css/main.6cd66d6c.chunk.css"
+  },
+  {
+    "revision": "72c6be1a7064563726caf18a1683e5da",
+    "url": "/index.html"
+  }
+];
