9387a7705450b1903d2b3885d8695e0d3881f29a
nadjayanna <nadjayannabr@gmail.com>
2018-11-17 18:40:52 +0100

    Updates

---
+++ precache-manifest.831d52647270710468d22e3a4f88a80a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1627007f1dec8dd542c",
+    "url": "/neighborhood-map/static/js/runtime~main.f1627007.js"
+  },
+  {
+    "revision": "262ce8cea527b867d7b7",
+    "url": "/neighborhood-map/static/js/main.262ce8ce.chunk.js"
+  },
+  {
+    "revision": "ce3dc7c73d42ab04848e",
+    "url": "/neighborhood-map/static/js/1.ce3dc7c7.chunk.js"
+  },
+  {
+    "revision": "262ce8cea527b867d7b7",
+    "url": "/neighborhood-map/static/css/main.ef9eb8fd.chunk.css"
+  },
+  {
+    "revision": "7b01f8ad204cf830161aba5c9f89bafb",
+    "url": "/neighborhood-map/index.html"
+  }
+];
